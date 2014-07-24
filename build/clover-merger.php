dc40df5eed0fd84a3b75c562cc8318f77dfc75a9
GrG <gregory.besson@adfab.fr>
2014-07-24 14:42:47 +0200

    Clover merger

--- build/clover-merger.php
+++ build/clover-merger.php
@@ -4,9 +4,9 @@ $modules  = new SimpleXMLElement(file_get_contents(__DIR__.'/modules.xml'));
 
 $fileFound = false;
 foreach ($modules->modules[0] as $module) {
-
-    $inputFile  = __DIR__.'/../../'.$module->path.'/build/junit.xml';
     
+    $inputFile  = __DIR__.'/../../'.$module->path.'/build/clover.xml';
+
     if (!file_exists($inputFile)) {
         continue;
     }
@@ -15,7 +15,7 @@ foreach ($modules->modules[0] as $module) {
         $mergedFile = new DOMDocument();
         $mergedFile->load($inputFile);
         
-        $res = $mergedFile->getElementsByTagName('testsuites')->item(0);
+        $res = $mergedFile->getElementsByTagName('project')->item(0);
         
         $fileFound = true;
         continue;
@@ -24,7 +24,7 @@ foreach ($modules->modules[0] as $module) {
     $doc = new DOMDocument();
     $doc->load($inputFile);
     
-    $items = $doc->getElementsByTagName('testsuite');
+    $items = $doc->getElementsByTagName('package');
     for ($i = 0; $i < $items->length; $i++) {
         $itemResult = $mergedFile->importNode($items->item($i), true);
         
@@ -33,6 +33,6 @@ foreach ($modules->modules[0] as $module) {
 }
 
 if ($fileFound){
-    $mergedFile->save(__DIR__.'/phpunit/junit.xml');    
+    $mergedFile->save(__DIR__.'/phpunit/clover.xml');    
 }
 
