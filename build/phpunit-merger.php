16a3e8e6722812c953660a1ffd7369f395e4b18b
GrG <gregory.besson@adfab.fr>
2014-07-24 11:02:11 +0200

    Première version de phpunit merger

---
+++ build/phpunit-merger.php
@@ -0,0 +1,38 @@
+<?php 
+
+$modules  = new SimpleXMLElement(file_get_contents(__DIR__.'/modules.xml'));
+
+$fileFound = false;
+foreach ($modules->modules[0] as $module) {
+
+    $inputFile  = __DIR__.'/../'.$module->path.'/build/junit.xml';
+    //$inputFile  = __DIR__.'/'.$module->name.'/junit.xml';
+    if (!file_exists($inputFile)) {
+        continue;
+    }
+    
+    if(!$fileFound){
+        $mergedFile = new DOMDocument();
+        $mergedFile->load($inputFile);
+        
+        $res = $mergedFile->getElementsByTagName('testsuites')->item(0);
+        
+        $fileFound = true;
+        continue;
+    }
+    
+    $doc = new DOMDocument();
+    $doc->load($inputFile);
+    
+    $items = $doc->getElementsByTagName('testsuite');
+    for ($i = 0; $i < $items->length; $i++) {
+        $itemResult = $mergedFile->importNode($items->item($i), true);
+        
+        $res->appendChild($itemResult);
+    }
+}
+
+if ($fileFound){
+    $mergedFile->save(__DIR__.'phpunit/phpunit-merged.xml');    
+}
+
